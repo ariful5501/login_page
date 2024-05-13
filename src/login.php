65fb5992c772a6f8c21b7de5f51e4befaa81ce6f
ariful5501 <arifulislam5501@gmail.com>
2024-05-13 19:45:34 +0600

    login page add

--- src/login.php
+++ src/login.php
@@ -1,7 +1,7 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Retrieve form data
-    $username = $_POST["username"];
+    
     $email = $_POST["email"];
     $password = $_POST["password"];
 
@@ -9,7 +9,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // For demonstration, let's just print the received data
     echo "<h1>Thank you for signing up!</h1>";
-    echo "<p>Username: $username</p>";
+   
     echo "<p>Email: $email</p>";
     // Do not display the password in real-life scenarios
 }
