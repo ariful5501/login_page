42b6024382fe53bae065ac2849b5e1a6e55fd227
ariful5501 <arifulislam5501@gmail.com>
2024-05-13 19:41:35 +0600

    php new page add

--- src/index.php
+++ src/index.php
@@ -90,19 +90,3 @@
 </body>
 </html>
 
-<!-- <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Retrieve form data
-    $username = $_POST["username"];
-    $email = $_POST["email"];
-    $password = $_POST["password"];
-
-    // Process your form data here, e.g., save to database
-
-    // For demonstration, let's just print the received data
-    echo "<h1>Thank you for signing up!</h1>";
-    echo "<p>Username: $username</p>";
-    echo "<p>Email: $email</p>";
-    // Do not display the password in real-life scenarios
-}
-?> -->
