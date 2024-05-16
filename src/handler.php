ea8117f29a82b88171e184bf5b50f0e17f40d5a5
ariful5501 <arifulislam5501@gmail.com>
2024-05-17 05:01:34 +0600

    javascript alart show add

--- src/handler.php
+++ src/handler.php
@@ -5,10 +5,22 @@ $email=$_REQUEST["email"];
 $password=$_REQUEST["password"];
 
 $sql = "INSERT INTO userinfo(name, email, password) VALUES ('$username','$email','$password')";
-mysqli_query($conn , $sql);
+// mysqli_query($conn , $sql);
 // echo "Data Added Successfully";
 
-header("Location:index.html");
-
+// Execute the query and check if it was successful
+if (mysqli_query($conn, $sql)) {
+    // If the query was successful, display a success alert
+    echo "<script type='text/javascript'>
+            alert('Data added successfully');
+            window.location.href = 'index.html';
+          </script>";
+} else {
+    // If the query failed, display an error alert
+    echo "<script type='text/javascript'>
+            alert('Error adding data: " . mysqli_error($conn) . "');
+            window.location.href = 'index.html';
+          </script>";
+}
 
 ?>
